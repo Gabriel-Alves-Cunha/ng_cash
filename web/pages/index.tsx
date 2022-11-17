3267c1aa7550313b9f8b59c3210f028cc8645ad5
Gabriel-Alves-Cunha <gabriel925486@outlook.com>
2022-11-17 11:20:39 -0300

    Almost finished with login and create user page.

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,8 +1,133 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
 import Image from "next/image";
-import Head from "next/head";
 
+import { baseUrl } from "lib/backend";
+
+import bgHome from "../public/bg-home.png";
+import logo from "../public/logo.svg";
+
+/** Página para realizar o cadastro ou login na NG informando username e password. */
 export default function Home() {
+	const [apiToCall, setApiToCall] = useState<"create-user" | "login">("login");
+	const router = useRouter();
+
+	async function loginOrSubscribe(e: React.FormEvent): Promise<void> {
+		e.preventDefault();
+
+		const formData = new FormData(e.target as HTMLFormElement);
+		const formEntries = {
+			plainTextPassword: formData.get("plainTextPassword"),
+			username: formData.get("username"),
+		};
+		const url = baseUrl + "/api/auth/" + apiToCall;
+
+		try {
+			const res = await fetch(url, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Accept: "application/json",
+				},
+				body: JSON.stringify(formEntries),
+			});
+
+			const json: LoginOrCreateUserResponse = await res.json();
+
+			if (json.message) console.error("Error =", json.message);
+			if (json.token) console.log("Token = ", json.token);
+
+			// router.push();
+		} catch (error) {
+			console.error("Error fetching " + url, error);
+
+			alert("There was an error!");
+		}
+	}
+
+	// Design from figma: "https://www.figma.com/file/4FgYu4Bdfhz9XyJadiOqdj/Login-View-(Community)?t=VRhzlhdKKvmX04gw-6".
+
 	return (
-		<div className=""></div>
+		<main className="flex h-screen w-screen">
+			<div className="w-1/2 h-full">
+				<Image
+					alt="A mountain with an orange and blue sky."
+					className="h-full"
+					placeholder="blur"
+					src={bgHome}
+				/>
+			</div>
+
+			<div className="relative w-1/2 h-full flex flex-col justify-center items-center">
+				<Image
+					className="bg-black box-content p-3 w-24 h-16 rounded-xl"
+					alt="NG.Cash's logo, it's name with a risc below it."
+					src={logo}
+				/>
+
+				<form
+					className="flex flex-col w-[50%] mt-8"
+					onSubmit={loginOrSubscribe}
+					id="form"
+				>
+					<p className="font-bold text-2xl leading-9 text-primary">
+						{apiToCall === "login" ? "Faça login abaixo" : "Cadastre-se abaixo"}
+					</p>
+
+					<label className="flex flex-col text-secondary font-normal leading-5 mt-6">
+						Nome de usuário
+						<input
+							className="mt-3 h-12 border-lightgray rounded-[5px] border-solid border-[1px] px-5 py-2"
+							placeholder="Fulano da Silva"
+							name="username"
+							type="text"
+							max={100}
+							required
+							min={3}
+						/>
+					</label>
+
+					<label className="flex flex-col text-secondary font-normal leading-5 mt-6">
+						Senha
+						<input
+							className="mt-3 h-12 border-lightgray rounded-[5px] border-solid border-[1px] px-5 py-2"
+							name="plainTextPassword"
+							type="password"
+							required
+							max={30}
+							min={8}
+						/>
+					</label>
+
+					{apiToCall === "login" ? (
+						<button
+							className="mt-16 h-12 bg-button-primary text-white font-bold rounded-[5px] hover:bg-button-primary-hovered duration-200"
+							type="submit"
+						>
+							Entrar
+						</button>
+					) : (
+						<button
+							className="mt-16 h-12 bg-button-secondary text-white font-bold rounded-[5px] hover:bg-button-secondary-hovered duration-200"
+							type="submit"
+						>
+							Cadastrar
+						</button>
+					)}
+
+					<button
+						onClick={() =>
+							setApiToCall(apiToCall === "login" ? "create-user" : "login")
+						}
+						className="w-fit mt-5 underline text-primary text-sm"
+						type="button"
+					>
+						{apiToCall === "login" ? "Cadastre-se" : "Login"}
+					</button>
+				</form>
+			</div>
+		</main>
 	);
 }
+
+type LoginOrCreateUserResponse = { token?: string; message?: string };
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			textColor: {
+				primary: "#1A202C",
+				secondary: "#4A5568",
+			},
+			borderColor: {
+				lightgray: "#E8E8E8",
+			},
+			backgroundColor: {
+				"button-primary": "#04C45C",
+				"button-primary-hovered": "#06a14e",
+
+				"button-secondary": "#0497c4",
+				"button-secondary-hovered": "#048ab3",
+			},
+		},
 	},
 	plugins: [],
 };
